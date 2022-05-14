44edf4ff78e26b19b0c429b66b8f78c91af05fb0
Alexandr Filipchenko <72624729+Newbie-Alfi@users.noreply.github.com>
2022-05-14 12:41:04 +0300

    Revert "Api branch"

--- client/src/API/index.ts
+++ client/src/API/index.ts
@@ -1,11 +1,1 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL:
-    "https://yfapi.net/v8/finance/chart/AAPL?comparisons=MSFT%2C%5EVIX&range=1mo&region=US&interval=1d&lang=en&events=div%2Csplit",
-  timeout: 1000,
-  headers: {
-    "X-API-KEY": "0THJCRvny24mtnF7JiUMw4UiGNxydsmp3QSEQOxl",
-    accept: "application/json",
-  },
-});
+export const services = {};
