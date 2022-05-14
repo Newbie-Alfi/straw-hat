bf978ee1d5b2e341d3125a5b549138c5140c7e06
Романенков Михаил <mihgank@gmail.com>
2022-05-14 14:40:48 +0300

    one stock fetch

--- client/src/routes/pages/Instrument.tsx
+++ client/src/routes/pages/Instrument.tsx
@@ -1,4 +1,5 @@
 import { FC, useEffect, useState } from 'react';
+
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -11,6 +12,7 @@ import {
   ChartData,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
+import { services } from '../../API';
 
 ChartJS.register(
   CategoryScale,
@@ -22,22 +24,16 @@ ChartJS.register(
   Legend
 );
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+let labels;
 
-export const data = {
+export let data: any = {
   labels,
   datasets: [
     {
-      label: 'Dataset 1',
-      data: [1, 2, 3],
+      label: 'AAPL',
+      data: [],
       borderColor: 'rgb(255, 99, 132)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: 'Dataset 2',
-      data: [3, 4, 5],
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
     },
   ],
 };
@@ -59,7 +55,15 @@ export const Instrument: FC = () => {
   const [chartData, setChartData] =
     useState<ChartData<'line', (number | null)[], unknown>>(data);
 
-  useEffect(() => {}, []);
+  const fetchData = async () => {
+    const APIResult = await services.chart.get();
+    labels = APIResult["AAPL"].timestamp;
+    data.datasets[0].data.push( APIResult["AAPL"].close);
+    setChartData(data)
+};
+
+
+  useEffect(() => { fetchData() }, []);
 
   return <Line options={options} data={chartData} />;
 };
