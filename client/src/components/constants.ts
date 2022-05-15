1d8f707239dd17822c94a47d82fd9f7a05ffb62a
mihgasnk <mihgank@gmail.com>
2022-05-15 11:30:33 +0300

    small fix

--- client/src/components/constants.ts
+++ client/src/components/constants.ts
@@ -1,59 +1,67 @@
 export const RANGE = [
   {
-    label: 'день',
-    value: '1d',
+    label: "день",
+    value: "1d",
   },
   {
-    label: '5 дней',
-    value: '5d',
+    label: "5 дней",
+    value: "5d",
   },
   {
-    label: 'месяц',
-    value: '1mo',
+    label: "месяц",
+    value: "1mo",
   },
   {
-    label: '3 месяца',
-    value: '3mo',
+    label: "3 месяца",
+    value: "3mo",
   },
   {
-    label: '6 месяцев',
-    value: '6mo',
+    label: "6 месяцев",
+    value: "6mo",
   },
   {
-    label: 'год',
-    value: '1y',
+    label: "год",
+    value: "1y",
   },
   {
-    label: '5 лет',
-    value: '5y',
+    label: "5 лет",
+    value: "5y",
+  },
+  {
+    label: "10 лет",
+    value: "10y",
+  },
+  {
+    label: "весь период",
+    value: "max",
   },
 ];
 
 export const INTERVAL = [
   {
-    label: 'минута',
-    value: '1m',
+    label: "минута",
+    value: "1m",
   },
   {
-    label: '5 минут',
-    value: '5m',
+    label: "5 минут",
+    value: "5m",
   },
   {
-    label: '15 минут',
-    value: '15m',
+    label: "15 минут",
+    value: "15m",
   },
   {
-    label: 'день',
-    value: '1d',
+    label: "день",
+    value: "1d",
   },
   {
-    label: 'неделя',
-    value: '1wk',
+    label: "неделя",
+    value: "1wk",
   },
   {
-    label: 'месяц',
-    value: '1mo',
+    label: "месяц",
+    value: "1mo",
   },
 ];
 
-export const REGIONS = ['US', 'AU', 'CA', 'FR', 'DE', 'HK', 'IT'];
+export const REGIONS = ["US", "AU", "CA", "FR", "DE", "HK", "IT"];
